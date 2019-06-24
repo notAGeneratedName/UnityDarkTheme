16080945db87341b45bb2e4cc961a394558ab747
notAGeneratedName <notsogreat@gmx.com>
2019-06-24 22:21:05 +0200

    Add Entire Project

---
+++ UnityDarkTheme/Assets/Scripts/AnimatorParameterBehaviour.cs
@@ -0,0 +1,180 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System;
+using UnityEngine;
+
+[Serializable]
+public class AnimatorParameter
+{
+    public string name;
+    public int ID;
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(AnimatorParameter))]
+    private class AnimatorParameterDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => property.isExpanded ? 45 : 15;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var name = property.FindPropertyRelative("name");
+            var ID = property.FindPropertyRelative("ID");
+
+            position.height = 15;
+            if (!(property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, property.displayName, true))) return;
+            position.x += 15;
+            position.y += 15;
+            EditorGUI.PropertyField(position, name);
+            ID.intValue = Animator.StringToHash(name.stringValue);
+            position.y += 15;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, ID);
+            GUI.enabled = true;
+            position.x -= 15;
+
+            if (property.serializedObject.hasModifiedProperties)
+                property.serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
+}
+
+public class AnimatorParameterBehaviour : MonoBehaviour
+{
+    public Animator animator;
+    public bool ignoreInactive;
+    public int parameterIndex;
+    public AnimatorParameter[] parameters;
+
+    private bool CanSet() => ignoreInactive || gameObject.activeSelf;
+
+    #region Setters
+
+    #region Trigger
+
+    public void SetTrigger() => SetTrigger(parameterIndex);
+
+    public void SetTrigger(int index)
+    {
+        if (index < parameters.Length)
+            SetTrigger(parameters[index]);
+    }
+
+    public void SetTrigger(AnimatorParameter param) => SetTrigger(animator, param);
+    public void SetTrigger(Animator anim, int index) => SetTrigger(anim, parameters[index]);
+    public void SetTrigger(Animator anim, AnimatorParameter param) => SetAnimatorTrigger(anim, param.ID);
+
+    private void SetAnimatorTrigger(Animator anim, int triggerID)
+    {
+        if (CanSet())
+            anim.SetTrigger(triggerID);
+    }
+
+    #endregion /Trigger
+
+    #region Bool
+
+    public void SetBool(bool value) => SetBool(parameterIndex, value);
+
+    public void SetBool(int index, bool value)
+    {
+        if (index < parameters.Length)
+            SetBool(parameters[index], value);
+    }
+
+    public void SetBool(AnimatorParameter param, bool value) => SetBool(animator, param, value);
+    public void SetBool(Animator anim, int index, bool value) => SetBool(anim, parameters[index], value);
+    public void SetBool(Animator anim, AnimatorParameter param, bool value) => SetAnimatorBool(anim, param.ID, value);
+
+    private void SetAnimatorBool(Animator anim, int boolID, bool value)
+    {
+        if (CanSet())
+            anim.SetBool(boolID, value);
+    }
+
+    #endregion /Bool
+
+    #region Integer
+
+    public void SetInteger(int value) => SetInteger(parameterIndex, value);
+
+    public void SetInteger(int index, int value)
+    {
+        if (index < parameters.Length)
+            SetInteger(parameters[index], value);
+    }
+
+    public void SetInteger(AnimatorParameter param, int value) => SetInteger(animator, param, value);
+    public void SetInteger(Animator anim, int index, int value) => SetInteger(anim, parameters[index], value);
+    public void SetInteger(Animator anim, AnimatorParameter param, int value) => SetAnimatorInteger(anim, param.ID, value);
+
+    private void SetAnimatorInteger(Animator anim, int integerID, int value)
+    {
+        if (CanSet())
+            anim.SetInteger(integerID, value);
+    }
+
+    #endregion /Integer
+
+    #region Float
+
+    public void SetFloat(float value) => SetFloat(parameterIndex, value);
+
+    public void SetFloat(int index, float value)
+    {
+        if (index < parameters.Length)
+            SetFloat(parameters[index], value);
+    }
+
+    public void SetFloat(AnimatorParameter param, float value) => SetFloat(animator, param, value);
+    public void SetFloat(Animator anim, int index, float value) => SetFloat(anim, parameters[index], value);
+    public void SetFloat(Animator anim, AnimatorParameter param, float value) => SetAnimatorFloat(anim, param.ID, value);
+
+    private void SetAnimatorFloat(Animator anim, int floatID, float value)
+    {
+        if (CanSet())
+            anim.SetFloat(floatID, value);
+    }
+
+    #endregion /Float
+
+    #endregion /Trigger
+
+    #region Getters
+
+    #region Bool
+
+    public bool GetBool() => GetBool(parameterIndex);
+    public bool GetBool(int index) => index < parameters.Length && GetBool(parameters[index]);
+    public bool GetBool(AnimatorParameter param) => GetBool(animator, param);
+    public bool GetBool(Animator anim, int index) => GetBool(anim, parameters[index]);
+    public bool GetBool(Animator anim, AnimatorParameter param) => GetAnimatorBool(anim, param.ID);
+    private static bool GetAnimatorBool(Animator anim, int boolID) => anim.GetBool(boolID);
+
+    #endregion
+
+    #region Integer
+
+    public int GetInteger() => GetInteger(parameterIndex);
+    public int GetInteger(int index) => index < parameters.Length ? GetInteger(parameters[index]) : 0;
+    public int GetInteger(AnimatorParameter param) => GetInteger(animator, param);
+    public int GetInteger(Animator anim, int index) => GetInteger(anim, parameters[index]);
+    public int GetInteger(Animator anim, AnimatorParameter param) => GetAnimatorInteger(anim, param.ID);
+    private static int GetAnimatorInteger(Animator anim, int intID) => anim.GetInteger(intID);
+
+    #endregion /Integer
+
+    #region Float
+
+    public float GetFloat() => GetFloat(parameterIndex);
+    public float GetFloat(int index) => index < parameters.Length ? GetFloat(parameters[index]) : 0;
+    public float GetFloat(AnimatorParameter param) => GetFloat(animator, param);
+    public float GetFloat(Animator anim, int index) => GetFloat(anim, parameters[index]);
+    public float GetFloat(Animator anim, AnimatorParameter param) => GetAnimatorFloat(anim, param.ID);
+    private static float GetAnimatorFloat(Animator anim, int intID) => anim.GetFloat(intID);
+
+    #endregion /Float
+
+    #endregion /Getters
+}
---
+++ UnityDarkTheme/Assets/Scripts/UnityVersion.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityVersion : MonoBehaviour
+{
+    [SerializeField] private Toggle _toggle;
+    [SerializeField] private TextMeshProUGUI _label;
+    [SerializeField] private Outline _outline;
+    [SerializeField] private Image _logoImage, _backgroundImage;
+    [SerializeField] private Color32 _unpatchedColor, _patchedColor, _selectedUnpatchedColor, _selectedPatchedColor;
+
+    private bool m_patched;
+    private Version m_version;
+    private string m_path;
+    private UnityDarkTheme m_unityDarkTheme;
+
+    public void Init(bool patched, Version version, string versionName, string path, ToggleGroup group, UnityDarkTheme unityDarkTheme)
+    {
+        SetPatched(patched);
+        m_version = version;
+        _label.text = versionName;
+        m_path = path;
+        _toggle.group = group;
+        m_unityDarkTheme = unityDarkTheme;
+    }
+
+    public void Select(bool selected)
+    {
+        SelectGUI(selected);
+        if (m_unityDarkTheme == null) return;
+        if (selected)
+            m_unityDarkTheme.SelectVersion(m_patched, m_version, m_path, this);
+        else
+            m_unityDarkTheme.SetButtonsInteractables(false);
+    }
+
+    public void SetPatched(bool patched)
+    {
+        m_patched = patched;
+        _backgroundImage.color = patched ? _patchedColor : _unpatchedColor;
+        _logoImage.color = patched ? _unpatchedColor : _patchedColor;
+        _label.color = patched ? _unpatchedColor : _patchedColor;
+        _toggle.isOn = false;
+    }
+
+    public void SelectGUI(bool selected)
+    {
+        _outline.enabled = selected;
+        _backgroundImage.color = m_patched ? selected ? _selectedPatchedColor : _patchedColor :
+            selected ? _selectedUnpatchedColor : _unpatchedColor;
+    }
+}
